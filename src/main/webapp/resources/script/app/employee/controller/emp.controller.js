77d07cb911ffaca7f9dfd745c0acdc3ea04c5b9f
jeevan-patil <jbkpatil@gmail.com>
2015-03-08 00:07:49 +0530

    list of users populated on employee tab

--- src/main/webapp/resources/script/app/employee/controller/emp.controller.js
+++ src/main/webapp/resources/script/app/employee/controller/emp.controller.js
@@ -1,5 +1,16 @@
 'use strict';
 
-angular.module('orgInfoApp').controller('EmpCtrl', function($scope) {
+angular.module('orgInfoApp').controller('EmpCtrl', [ '$scope', '$resource', '$http', function($scope, $resource, $http) {
 	$scope.message = "List of Employees will be shown here.";
-});
+	$scope.users = [];
+
+	$scope.$on('$viewContentLoaded', function(){
+		$scope.listUsers();
+	});
+
+	$scope.listUsers = function() {
+    	$http.get('users').success(function(data) {
+    		$scope.users = data;
+    	});
+    };
+}]);
--- src/main/webapp/resources/script/app/org/controller/orglist.controller.js
+++ src/main/webapp/resources/script/app/org/controller/orglist.controller.js
@@ -2,13 +2,19 @@
 
 angular.module('orgInfoApp').controller('OrganizationCtrl', [ '$scope', '$resource', '$http', function($scope, $resource, $http) {
 	$scope.message = "Information about organization will be shown here.";
-	$scope.neworg = [];
+	$scope.neworg = {};
 
 	$scope.createNewOrganization = function() {
 		var Org = $resource('org/create');
 		Org.save($scope.neworg, function(response) {
-			console.log(respones);
+			console.log(response);
 		});
+
+/*		$http.post('org/create', $scope.neworg).success(function(data, status, headers, config) {
+			console.log(status);
+		})
+		.error(function(data, status, headers, config) {
+		});*/
     };
 
     $scope.listUsers = function() {
